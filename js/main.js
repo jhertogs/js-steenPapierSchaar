78fb83285f534677c082d9817320b2b39436191f
Josse Hertogs <jossehertogs@gmail.com>
2024-04-22 15:33:45 +0200

    dynamically display images

--- js/main.js
+++ js/main.js
@@ -3,27 +3,29 @@ const paper = document.getElementById("paper")
 const scissors = document.getElementById("scissors")
 
 const rps =  {
-    1: "rock",
-    2: "paper",
-    3: "scissors"
+    1: "steen",
+    2: "papier",
+    3: "schaar"
 }
 
 rock.addEventListener("click", function() {
     document.getElementById("message_box").innerHTML = "Je kiest: steen!!"
-    document.getElementById("player").style.backgroundImage = "url('http://localhost/js%20steenpapierschaar/images/rock.png')"
+    document.getElementById("player").style.backgroundImage = "url('http://localhost/js%20steenpapierschaar/images/steen.png')"
+
     let randomNumber = Math.floor(Math.random() * 3) + 1
-    
     const computerChoice = rps[randomNumber]
     document.getElementById("message_box").innerHTML += "  Computer kiest: " + computerChoice
-    if(computerChoice == "paper") {
+    document.getElementById("PC").style.backgroundImage = "url('http://localhost/js%20steenpapierschaar/images/" + computerChoice + ".png')"
+
+    if(computerChoice == "papier") {
         document.getElementById("status-msg").innerHTML = "<h3> Je hebt verloren!</h3>"
         document.getElementById("status-msg").style.backgroundColor = 'rgb(239, 65, 22)'
     }
-    if (computerChoice == "scissors"){
+    if (computerChoice == "schaar"){
         document.getElementById("status-msg").innerHTML = "<h3> Je hebt gewonnen!</h3>"
         document.getElementById("status-msg").style.backgroundColor = 'rgb(38, 253, 110)'
     }
-    if (computerChoice == "rock") {
+    if (computerChoice == "steen") {
         document.getElementById("status-msg").innerHTML = "<h3> Het is gelijkspel!</h3>"
         document.getElementById("status-msg").style.backgroundColor = 'rgb(49, 231, 251)'
     }
@@ -32,19 +34,21 @@ rock.addEventListener("click", function() {
 
 paper.addEventListener("click", function() {
     document.getElementById("message_box").innerHTML = "Je kiest: papier!!"
+    document.getElementById("player").style.backgroundImage = "url('http://localhost/js%20steenpapierschaar/images/papier.png')"
     let randomNumber = Math.floor(Math.random() * 3) + 1
     const computerChoice = rps[randomNumber]
     document.getElementById("message_box").innerHTML += "  Computer kiest: " + computerChoice
+    document.getElementById("PC").style.backgroundImage = "url('http://localhost/js%20steenpapierschaar/images/" + computerChoice + ".png')"
 
-    if(computerChoice == "paper") {
+    if(computerChoice == "papier") {
         document.getElementById("status-msg").innerHTML = "<h3> Het is gelijkspel!</h3>"
         document.getElementById("status-msg").style.backgroundColor = 'rgb(49, 231, 251)'
     }
-    if (computerChoice == "scissors"){
+    if (computerChoice == "schaar"){
         document.getElementById("status-msg").innerHTML = "<h3> Je hebt verloren!</h3>"
         document.getElementById("status-msg").style.backgroundColor = 'rgb(239, 65, 22)'
     }
-    if (computerChoice == "rock") {
+    if (computerChoice == "steen") {
         document.getElementById("status-msg").innerHTML = "<h3> Je hebt gewonnen!</h3>"
         document.getElementById("status-msg").style.backgroundColor = 'rgb(38, 253, 110)'
     }
@@ -52,19 +56,21 @@ paper.addEventListener("click", function() {
 
 scissors.addEventListener("click", function() {
     document.getElementById("message_box").innerHTML = "Je kiest: schaar!!"
+    document.getElementById("player").style.backgroundImage = "url('http://localhost/js%20steenpapierschaar/images/schaar.png')"
     let randomNumber = Math.floor(Math.random() * 3) + 1
     const computerChoice = rps[randomNumber]
     document.getElementById("message_box").innerHTML += "  Computer kiest: " + computerChoice
+    document.getElementById("PC").style.backgroundImage = "url('http://localhost/js%20steenpapierschaar/images/" + computerChoice + ".png')"
 
-    if(computerChoice == "scissors") {
+    if(computerChoice == "schaar") {
         document.getElementById("status-msg").innerHTML = "<h3> Het is gelijkspel!</h3>"
         document.getElementById("status-msg").style.backgroundColor = 'rgb(49, 231, 251)'
     }
-    if (computerChoice == "rock"){
+    if (computerChoice == "steen"){
         document.getElementById("status-msg").innerHTML = "<h3> Je hebt verloren!</h3>"
         document.getElementById("status-msg").style.backgroundColor = 'rgb(239, 65, 22)'
     }
-    if (computerChoice == "paper") {
+    if (computerChoice == "papier") {
         document.getElementById("status-msg").innerHTML = "<h3> Je hebt gewonnen!</h3>"
         document.getElementById("status-msg").style.backgroundColor = 'rgb(38, 253, 110)'
     }
