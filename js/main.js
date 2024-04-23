45f591a43c5f9c8ee0f03cab12bdb2cac5428dfe
Josse Hertogs <jossehertogs@gmail.com>
2024-04-23 11:25:44 +0200

    het spel is bijna klaar. (spel heeft nu 3 rondes en je wint als je meer dan 2 keer het juiste wapen gekozen hebt. Ook heeft het spel betere styling nu)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const rock = document.getElementById("rock")
 const paper = document.getElementById("paper")
 const scissors = document.getElementById("scissors")
+let ronde = 0;
+let gewonnen = 0;
 
 const rps =  {
     1: "steen",
@@ -8,7 +10,21 @@ const rps =  {
     3: "schaar"
 }
 
+
+
+
 rock.addEventListener("click", function() {
+
+    if (ronde >= 3){
+        if (gewonnen >= 2){
+            document.getElementById("status-msg").innerHTML = "<h3> Je hebt gewonnen!</h3>"
+            document.getElementById("status-msg").style.backgroundColor = 'rgb(38, 253, 110)'
+        }else{
+            document.getElementById("status-msg").innerHTML = "<h3> Je hebt verloren!</h3>"
+            document.getElementById("status-msg").style.backgroundColor = 'rgb(239, 65, 22)'
+        }
+        return;
+    }
     document.getElementById("message_box").innerHTML = "Je kiest: steen!!"
     document.getElementById("player").style.backgroundImage = "url('http://localhost/js%20steenpapierschaar/images/steen.png')"
 
@@ -18,21 +34,29 @@ rock.addEventListener("click", function() {
     document.getElementById("PC").style.backgroundImage = "url('http://localhost/js%20steenpapierschaar/images/" + computerChoice + ".png')"
 
     if(computerChoice == "papier") {
-        document.getElementById("status-msg").innerHTML = "<h3> Je hebt verloren!</h3>"
-        document.getElementById("status-msg").style.backgroundColor = 'rgb(239, 65, 22)'
+        gewonnen--
     }
     if (computerChoice == "schaar"){
-        document.getElementById("status-msg").innerHTML = "<h3> Je hebt gewonnen!</h3>"
-        document.getElementById("status-msg").style.backgroundColor = 'rgb(38, 253, 110)'
+        gewonnen++
     }
-    if (computerChoice == "steen") {
-        document.getElementById("status-msg").innerHTML = "<h3> Het is gelijkspel!</h3>"
-        document.getElementById("status-msg").style.backgroundColor = 'rgb(49, 231, 251)'
-    }
-     
+    
+   
+    ronde++
+    document.getElementById("ronde-box").innerHTML = "Ronde: " + ronde + "."
 })
 
 paper.addEventListener("click", function() {
+
+    if (ronde >= 3){
+        if (gewonnen >= 2){
+            document.getElementById("status-msg").innerHTML = "<h3> Je hebt gewonnen!</h3>"
+            document.getElementById("status-msg").style.backgroundColor = 'rgb(38, 253, 110)'
+        }else{
+            document.getElementById("status-msg").innerHTML = "<h3> Je hebt verloren!</h3>"
+            document.getElementById("status-msg").style.backgroundColor = 'rgb(239, 65, 22)'
+        }
+        return;
+    }
     document.getElementById("message_box").innerHTML = "Je kiest: papier!!"
     document.getElementById("player").style.backgroundImage = "url('http://localhost/js%20steenpapierschaar/images/papier.png')"
     let randomNumber = Math.floor(Math.random() * 3) + 1
@@ -40,21 +64,31 @@ paper.addEventListener("click", function() {
     document.getElementById("message_box").innerHTML += "  Computer kiest: " + computerChoice
     document.getElementById("PC").style.backgroundImage = "url('http://localhost/js%20steenpapierschaar/images/" + computerChoice + ".png')"
 
-    if(computerChoice == "papier") {
-        document.getElementById("status-msg").innerHTML = "<h3> Het is gelijkspel!</h3>"
-        document.getElementById("status-msg").style.backgroundColor = 'rgb(49, 231, 251)'
-    }
     if (computerChoice == "schaar"){
-        document.getElementById("status-msg").innerHTML = "<h3> Je hebt verloren!</h3>"
-        document.getElementById("status-msg").style.backgroundColor = 'rgb(239, 65, 22)'
+       
+        gewonnen--
     }
     if (computerChoice == "steen") {
-        document.getElementById("status-msg").innerHTML = "<h3> Je hebt gewonnen!</h3>"
-        document.getElementById("status-msg").style.backgroundColor = 'rgb(38, 253, 110)'
+        
+        gewonnen++
     }
+    
+    ronde++
+    document.getElementById("ronde-box").innerHTML = "Ronde: " + ronde + "."
 })
 
 scissors.addEventListener("click", function() {
+
+    if (ronde >= 3){
+        if (gewonnen >= 2){
+            document.getElementById("status-msg").innerHTML = "<h3> Je hebt gewonnen!</h3>"
+            document.getElementById("status-msg").style.backgroundColor = 'rgb(38, 253, 110)'
+        }else{
+            document.getElementById("status-msg").innerHTML = "<h3> Je hebt verloren!</h3>"
+            document.getElementById("status-msg").style.backgroundColor = 'rgb(239, 65, 22)'
+        }
+        return;
+    }
     document.getElementById("message_box").innerHTML = "Je kiest: schaar!!"
     document.getElementById("player").style.backgroundImage = "url('http://localhost/js%20steenpapierschaar/images/schaar.png')"
     let randomNumber = Math.floor(Math.random() * 3) + 1
@@ -62,18 +96,16 @@ scissors.addEventListener("click", function() {
     document.getElementById("message_box").innerHTML += "  Computer kiest: " + computerChoice
     document.getElementById("PC").style.backgroundImage = "url('http://localhost/js%20steenpapierschaar/images/" + computerChoice + ".png')"
 
-    if(computerChoice == "schaar") {
-        document.getElementById("status-msg").innerHTML = "<h3> Het is gelijkspel!</h3>"
-        document.getElementById("status-msg").style.backgroundColor = 'rgb(49, 231, 251)'
-    }
     if (computerChoice == "steen"){
-        document.getElementById("status-msg").innerHTML = "<h3> Je hebt verloren!</h3>"
-        document.getElementById("status-msg").style.backgroundColor = 'rgb(239, 65, 22)'
+        gewonnen--
     }
     if (computerChoice == "papier") {
-        document.getElementById("status-msg").innerHTML = "<h3> Je hebt gewonnen!</h3>"
-        document.getElementById("status-msg").style.backgroundColor = 'rgb(38, 253, 110)'
+        gewonnen++
     }
+    
+    
+    ronde++
+    document.getElementById("ronde-box").innerHTML = "Ronde: " + ronde + "."
 })
 
 
